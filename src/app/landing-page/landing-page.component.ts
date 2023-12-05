f864cb3834d4e16bbd1486d76122a786f7855de5
brauliogm <brauliogm16@gmail.com>
2023-12-04 20:31:01 -0600

    Agrego la landing que al final termine en dodonde iba a hacer mi portafolio, ya quedo todo aqui

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-landing-page',
+  selector: 'app-root',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
+  title = 'portafolio';
 
+  logoPrincipal: SafeResourceUrl;
+  ilustracionIntro: SafeResourceUrl;
+  peopleAliBravo: SafeResourceUrl;
+  iconFacebook: SafeResourceUrl;
+  iconYoutube: SafeResourceUrl;
+  iconTwitter: SafeResourceUrl;
+  iconPinterest: SafeResourceUrl;
+  iconInstagram: SafeResourceUrl;
+  logoWhite: SafeResourceUrl;
+  sombra: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    // Ruta relativa a la imagen en la carpeta assets
+    const rutaImagen = 'assets/logo.svg';
+    const ilustracionIntro1 = 'assets/illustration-intro.svg';
+    const avatarAliBravo = 'assets/avatar-ali.png';
+    const iconFacebook1 = 'assets/icon-facebook.svg';
+    const iconYoutube1 = 'assets/icon-youtube.svg';
+    const iconTwitter1 = 'assets/icon-twitter.svg';
+    const iconPinterest1 = 'assets/icon-pinterest.svg';
+    const iconInstagram1 = 'assets/icon-instagram.svg';
+    const logoWhite1 = 'assets/logo.svg';
+    const sombra1 = 'assets/bg-tablet-pattern.svg';
+
+    // Sanitizar la URL de la imagen para evitar problemas de seguridad
+    this.logoPrincipal = this.sanitizer.bypassSecurityTrustResourceUrl(rutaImagen);
+    this.ilustracionIntro = this.sanitizer.bypassSecurityTrustResourceUrl(ilustracionIntro1);
+    this.peopleAliBravo = this.sanitizer.bypassSecurityTrustResourceUrl(avatarAliBravo);
+    this.iconFacebook = this.sanitizer.bypassSecurityTrustResourceUrl(iconFacebook1);
+    this.iconYoutube = this.sanitizer.bypassSecurityTrustResourceUrl(iconYoutube1);
+    this.iconTwitter = this.sanitizer.bypassSecurityTrustResourceUrl(iconTwitter1);
+    this.iconPinterest = this.sanitizer.bypassSecurityTrustResourceUrl(iconPinterest1);
+    this.iconInstagram = this.sanitizer.bypassSecurityTrustResourceUrl(iconInstagram1);
+    this.logoWhite = this.sanitizer.bypassSecurityTrustResourceUrl(logoWhite1);
+    this.sombra = this.sanitizer.bypassSecurityTrustResourceUrl(sombra1);
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,22 @@ module.exports = {
     extend: {
 
       fontFamily: {
-        "principal" : ['"Be Vietnam Pro"', 'sans-serif']
+        "principal": ['"Be Vietnam Pro"', 'sans-serif']
       },
 
       colors: {
-        'bright-red': 'hsl(12, 88%, 59%)',
-        'dark-blue': 'hsl(228, 39%, 23%)'
+        "bright-red": "hsl(12, 88%, 59%)",
+        "dark-blue": "hsl(228, 39%, 23%)",
+        "dark-grayish-blue": "hsl(227, 12%, 61%)",
+        "very-dark-blue": "hsl(233, 12%, 13%)",
+        "very-pale-red": "hsl(13, 100%, 96%)",
+        "vary-light-gray": "hsl(0, 0%, 98%)",
       },
 
+      backgroundImage: {
+        "close-menu": "url('../src/assets/icon-close.svg')",
+        "open-menu": "url('../src/assets/icon-hamburger.svg')",
+      }
     },
   },
   plugins: [],
